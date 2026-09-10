13490c06ab5c0ccb718286ce9838097472e8e507
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-09-10 08:44:53 -0600

    T2.6.1 Feat: Se agregaron las consultas sobre ventas al dia y demas caracteristicas en DAO e Impl para el funcionamiento de las demas ramas

--- src/org/paginalib3/dao/VentaDAO.java
+++ src/org/paginalib3/dao/VentaDAO.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package org.paginalib3.dao;
 
-/**
- *
- * @author informatica
- */
+import java.sql.SQLException;
+import java.util.List;
+import org.paginalib3.model.DetalleVenta;
+import org.paginalib3.model.Venta;
+
 public interface VentaDAO {
-    
+    boolean registrarVenta(Venta venta, List<DetalleVenta> detalles) throws SQLException;
+    boolean registrarVenta(Venta venta, List<DetalleVenta> detalles, Integer usuarioAutoriza) throws SQLException;
+    Venta buscarPorId(int idVenta) throws SQLException;
+    List<Venta> listarVentasDelDiaPorUsuario(int idUsuario) throws SQLException;
+    boolean anularVenta(int idVenta, int idUsuario, String motivo) throws SQLException;
+    boolean devolverVenta(int idVenta, int idUsuario, String motivo) throws SQLException;
 }
