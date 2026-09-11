f06c44b121f7ef1696b92b2207c0d76903520a6c
Juan Carlos <jestrada-2026061@kinal.edu.gt>
2026-09-11 11:15:45 -0600

    T3 Fix: Se modifico el dashboard de BodegaController

--- src/org/paginalib3/controller/DashboardBodegaController.java
+++ src/org/paginalib3/controller/DashboardBodegaController.java
@@ -1,6 +1,15 @@
 package org.paginalib3.controller;
 
+import javafx.fxml.FXML;
+import org.paginalib3.system.Main;
+
 public class DashboardBodegaController extends DashboardBaseController {
+
+    @FXML
+    private void irASalidaInventario() {
+        Main.cambiarVista("/org/paginalib3/view/salida_inventario.fxml",
+                "Pagina-Libreria | Registrar salida de inventario", 1200, 760);
+    }
 
     @Override
     protected String rolPermitido() {
