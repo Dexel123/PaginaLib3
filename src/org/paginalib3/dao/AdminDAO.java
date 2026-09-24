f9f6e8610a993121367ea48f2f4014cbc1b1aa38
Christian Garcia <cgarcia-2023529@kinal.edu.gt>
2026-09-24 08:41:41 -0600

    T4.1.2 Feat: Implementamos obtener ventas totales/históricas desde el origen de indicadores

---
+++ src/org/paginalib3/dao/AdminDAO.java
@@ -0,0 +1,8 @@
+package org.paginalib3.dao;
+
+import java.sql.SQLException;
+import org.paginalib3.model.DashboardIndicadores;
+
+public interface AdminDAO {
+    DashboardIndicadores obtenerIndicadores() throws SQLException;
+}
---
+++ src/org/paginalib3/dao/impl/AdminDAOImpl.java
@@ -0,0 +1,31 @@
+package org.paginalib3.dao.impl;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.paginalib3.dao.AdminDAO;
+import org.paginalib3.model.DashboardIndicadores;
+import org.paginalib3.util.Conexion;
+
+public class AdminDAOImpl implements AdminDAO {
+    @Override
+    public DashboardIndicadores obtenerIndicadores() throws SQLException {
+        try (Connection c = Conexion.getInstancia().conectar();
+             CallableStatement s = c.prepareCall("{CALL sp_dashboardadmin()}");
+             ResultSet r = s.executeQuery()) {
+            DashboardIndicadores d = new DashboardIndicadores();
+            if (r.next()) {
+                d.setVentasTotales(r.getDouble("ventas_totales"));
+                d.setVentasHoy(r.getDouble("ventas_hoy"));
+                d.setTransaccionesHoy(r.getInt("transacciones_hoy"));
+                d.setLibrosActivos(r.getInt("libros_activos"));
+                d.setUnidadesEnStock(r.getInt("unidades_en_stock"));
+                d.setInventarioValorizadoCosto(r.getDouble("inventario_valorizado_costo"));
+                d.setUsuariosActivos(r.getInt("usuarios_activos"));
+                d.setLibrosStockCritico(r.getInt("libros_stock_critico"));
+            }
+            return d;
+        }
+    }
+}
